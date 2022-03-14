cb78eb88c6447749ce8b0288a6b184c8e08b293a
username-taken-512 <jonatan.engerdahl@gmail.com>
2022-03-14 20:55:30 +0100

    -'Today' container in start page now dynamically loads screenings for today and links screeningId to Ticket page

--- frontend/js/screeningsToday.js
+++ frontend/js/screeningsToday.js
@@ -1,15 +1,15 @@
-// Loads today's screenings in the section to the right
-// WORK IN PROGRESS
+// Loads today's screenings in the section to the right of the frontpage
 
 async function getTodaysScreenings() {
   return await (await fetch('/api/VW_ScreeningsToday')).json();
 }
 
-async function loadTodayContainer() {
-  let screeningsToday = await getTodaysScreenings();
+// Fills the 'Today' container with screenings for each auditorium
+async function drawTodayContainer(screeningsToday) {
   let todayContainer = document.querySelector('.screenings-today');
   let html = '<h2>Today</h2>';
 
+  // Sort screenings into Map depending on auditorium as key, arrays of screenings as value
   const auditoriums = new Map();
   let auditoriumArray = [];
   for (element of screeningsToday) {
@@ -23,6 +23,27 @@ async function loadTodayContainer() {
     }
   };
 
-  console.log(screeningsToday);
-  console.log(auditoriums.keys());
+  // Loops map to create container for each auditorium and appends screening children
+  for (const [key, value] of auditoriums.entries()) {
+    html += `
+    <div class="screenings-today-auditorium">
+    <h3>${value[0].auditoriumName}</h3>
+    `;
+
+    value.forEach(element => {
+      html += `
+      <p class="screenings-today-item" title="${element.movieTitle}">
+        <a href="/tickets?screeningId=${element.id}">${element.date.substring(11)} - ${element.movieTitle}</a>
+      </p>
+      `;
+    });
+    html += '</div>';
+  }
+  todayContainer.innerHTML = html;
+}
+
+// Start method
+async function loadTodayContainer() {
+  let screeningsToday = await getTodaysScreenings();
+  drawTodayContainer(screeningsToday);
 }
